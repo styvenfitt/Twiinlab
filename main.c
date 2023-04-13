6e7d67be32ac3442e813701e19af67e691f84681
kevin Styven Grajales Galeano <styvenfitt@gmail.com>
2023-04-13 11:22:31 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Bienvenido al repositorio de Twiinlab!");
+
+   printf("A partir de ahora actualizaremos el estado del proyecto desde aquí!!");
+
+   printf("\nfinalizado, desplegar VMs");
+   printf("\nfinalizado, Actualizacion del catalogo de dispositivos");
+
+   printf("\nFinalizado, crear seccion de login");
+
+   printf("Finalizado, Añadir boton para eliminar permisos");
+   
+
+   printf("Añadido a To Do, Habilitar servicio Web");
+   printf("Añadido a To Do, Listar usuarios");
+   printf("Añadido a To Do, Descripcion y diseño de la API");
+   
+   printf("Añadido a REVIEW, Diseño UX/UI");
+
+   printf("\nEsta en proceso Doing Lógica backend de la web");
+
+
+   return 0;
+}
