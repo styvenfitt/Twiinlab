ab5c151d290d37dc7045b3858386326f82695e4e
MPG846 <128475739+MPG846@users.noreply.github.com>
2023-04-13 11:35:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ int main() {
    printf("Bienvenido al repositorio de Twiinlab!");
 
    printf("A partir de ahora actualizaremos el estado del proyecto desde aquí!!");
+   ////////////////////////////////////////
    printf("se crea un branch de ToDo");
-
+   ////////////////////////////////////////
+   print("Habilitar servicio web");
+   print("Listar usuarios");
+   print("Back de la seccion gestion usuarios");
+   
 
    return 0;
 }
